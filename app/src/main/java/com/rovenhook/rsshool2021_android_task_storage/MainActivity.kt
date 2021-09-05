c55bd52ea09e481faf81f9a6ad4a612ea6b2c32b
Rovenhook <stuchalka8@gmail.com>
2021-09-05 11:54:09 +0300

    initial commit

---
+++ app/src/main/java/com/rovenhook/rsshool2021_android_task_storage/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rovenhook.rsshool2021_android_task_storage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
